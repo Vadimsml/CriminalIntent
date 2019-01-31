7e72606f25f49368e3200bcc3759ce95418caff7
Vadimsml <vadimrk@email.ua>
2019-01-31 23:49:43 +0200

    chapter 9

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,9 +17,29 @@ public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
-        public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private Crime mCrime;
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+
+        public void bind(Crime crime){
+            mCrime=crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+        }
+
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent)  {
             super(inflater.inflate(R.layout.list_item_crim, parent, false));
+            itemView.setOnClickListener(this);
+            mTitleTextView=(TextView)itemView.findViewById(R.id.crime_title);
+            mDateTextView=(TextView)itemView.findViewById(R.id.crime_date);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(),mCrime.getTitle()+" clicked!", Toast.LENGTH_SHORT).show();
+
         }
     }
     @Override
@@ -54,7 +76,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(@NonNull CrimeHolder holder, int position) {
-
+            Crime crime = mCrimes.get(position);
+            holder.bind(crime);
         }
 
         @Override
